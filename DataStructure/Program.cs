22204df95ffb4759ea88f3f54134e4032e5808a8
aamir0143 <mdaamirreza00@gmail.com>
2022-02-05 01:01:36 +0530

    [Aamir14] Add : Delete the first element in the LinkedList

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,14 +8,14 @@ namespace DataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("==========Welcome To Data Structure Programs==========");
-
             Console.WriteLine("SEARCH OPERATIONS !");
             LinkedList list = new LinkedList();
             list.Add(56);
-            //list.Add(30);
+            list.Add(30);
             list.Add(70);
+            list.RemoveFirstNode();
+            Console.WriteLine("After removing is : ");
             list.Display();
-            list.InsertAtParticularPosition(2, 30);      
             Console.WriteLine("========================================================================================");
             Console.ReadLine();
         }
