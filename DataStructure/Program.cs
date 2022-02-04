c3a6a6acb15561d809b6c8818cced03567008a21
aamir0143 <mdaamirreza00@gmail.com>
2022-02-04 23:11:23 +0530

    [Aamir14] Add : Added Create Linked List By Appending

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,13 +11,9 @@ namespace DataStructure
 
             Console.WriteLine("SEARCH OPERATIONS !");
             LinkedList list = new LinkedList();
-            //list.Add(56);
-            //list.Add(30);
-            //list.Add(70);
-
-            list.AddReverse(70);
-            list.AddReverse(30);
-            list.AddReverse(56);
+            list.Appending(56); 
+            list.Appending(30);
+            list.Appending(70);
             Console.WriteLine("Display Nodes are");
             list.Display();
             Console.WriteLine("========================================================================================");
