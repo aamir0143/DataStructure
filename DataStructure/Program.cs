7a0be56d58cb39167f9cdee8ec2a79fee0171ccf
aamir0143 <mdaamirreza00@gmail.com>
2022-02-04 21:54:15 +0530

    [Aamir14] Add : Added Create a simple linked list

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To Data Structure Programs==========");
-Console.ReadLine();
+using System;
+
+namespace DataStructure 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Data Structure Programs==========");
+
+            Console.WriteLine("SEARCH OPERATIONS !");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("========================================================================================");
+            Console.ReadLine();
+        }
+    }
+}
