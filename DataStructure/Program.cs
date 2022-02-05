b3d1a7201255e8f07c78d3b50bec1c487c5f3dc0
aamir0143 <mdaamirreza00@gmail.com>
2022-02-06 00:17:07 +0530

    [Aamir14] Add : Delete the Node 40 from the LinkedList

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -12,10 +12,12 @@ namespace DataStructure
             LinkedList list = new LinkedList();
             list.Add(56);
             list.Add(30);
+            list.Add(40);
             list.Add(70);
-            int position = list.Search(30);
-            position++;
-            list.InsertAtParticularPosition(position, 40);
+            int position = list.Search(40);
+            list.DeleteNodeAtParticularPosition(position);
+            Console.WriteLine("\nAfter deleting 40 then Size and final sequence is: ");
+            list.Size();
             list.Display();
             Console.WriteLine("========================================================================================");
             Console.ReadLine();
